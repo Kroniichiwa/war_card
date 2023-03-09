70f1a55be87e5d4848ba3e34adb316a3a62242b6
6110450014 <Kritsada.lakhasont@gmail.com>
2023-03-09 15:44:51 +1100

    Initial Commit

---
+++ war_card/war_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_cardApp.swift
+//  war_card
+//
+//  Created by Kritsadalakhasont on 9/3/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct war_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
